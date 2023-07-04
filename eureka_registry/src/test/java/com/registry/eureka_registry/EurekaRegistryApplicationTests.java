d3458ed7d59a6e5edcba48987b88b8fdc50ea6cf
gauti4ru <gautam.hariom25@gmail.com>
2023-07-05 00:17:39 +0530

    Added eureka registry and clients

---
+++ eureka_registry/src/test/java/com/registry/eureka_registry/EurekaRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.registry.eureka_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
